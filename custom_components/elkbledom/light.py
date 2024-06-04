b3144f76814a052895644d98ed07f7da55f41992
connor-dobbins <cpdobbins18@gmail.com>
2024-06-04 15:33:22 -0500

    Update light.py
    made ColorMode.WHITE a warmer white

--- custom_components/elkbledom/light.py
+++ custom_components/elkbledom/light.py
@@ -111,7 +111,7 @@ class BLEDOMLight(LightEntity):
                 LOGGER.debug("Change color to white to reset led strip when other infrared control interact")
                 self._attr_color_mode = ColorMode.WHITE
                 self._attr_effect = None
-                await self._instance.set_color(self._transform_color_brightness((255, 255, 255), 250))
+                await self._instance.set_color(self._transform_color_brightness((255, 50, 5), 250))
         
         if ATTR_BRIGHTNESS in kwargs and kwargs[ATTR_BRIGHTNESS] != self.brightness and self.rgb_color != None:
             await self._instance.set_brightness(kwargs[ATTR_BRIGHTNESS])
